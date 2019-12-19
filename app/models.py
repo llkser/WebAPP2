4f6fe32128bd8f2893f611a6386486343695652b
Kang Liu <sc17kl@leeds.ac.uk>
2019-12-19 16:48:33 +0800

    base

---
+++ app/models.py
@@ -0,0 +1,24 @@
+from app import db
+
+class User(db.Model):
+    UserID=db.Column(db.Integer, primary_key=True)
+    UserName=db.Column(db.String(250))
+    Password=db.Column(db.String(250))
+    Email=db.Column(db.String(250))
+    Phonenumber=db.Column(db.String(250))
+    QQnumber=db.Column(db.String(250))
+    Avater=db.Column(db.String(250))
+    Authority=db.Column(db.Integer)
+
+class Book(db.Model):
+    BookID=db.Column(db.Integer, primary_key=True)
+    BookName=db.Column(db.String(250))
+    Description=db.Column(db.String(250))
+    Cover=db.Column(db.String(250))
+
+class Bookshelf(db.Model):
+    ShelfID=db.Column(db.Integer, primary_key=True)
+    UserID=db.Column(db.Integer)
+    BookID=db.Column(db.Integer)
+    Read=db.Column(db.Boolean, default=False)
+    Favor=db.Column(db.Boolean, default=False)
