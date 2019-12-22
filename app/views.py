4cff9cb714c59b106fa05a2f955d5d0602ffa009
Kang Liu <sc17kl@leeds.ac.uk>
2019-12-22 19:20:01 +0800

    bookshelf

--- app/views.py
+++ app/views.py
@@ -6,19 +6,53 @@ from flask.helpers import make_response
 
 @app.route("/", methods=['GET','POST'])
 def bookstore():
-    Bookstore=Book.query.order_by('BookName').all()
-    if 'username' in session:
-        return render_template('bookstore.html',
-                sidebar=1,
-                username=session['username'],
-                authority=session['authority'],
-                Bookstore=Bookstore
-        )
+    if request.method=='GET':
+        Bookstore=Book.query.order_by('BookName').all()
+        if 'username' in session:
+            userID=User.query.filter(User.UserName==session['username']).first().UserID
+            bookshelf=Bookshelf.query.filter(Bookshelf.UserID==userID)
+            shelf=[]
+            for i in bookshelf:
+                bookname=Book.query.filter(Book.BookID==i.BookID).first().BookName
+                shelf.append(bookname)
+            return render_template('bookstore.html',
+                    sidebar=1,
+                    username=session['username'],
+                    authority=session['authority'],
+                    Bookstore=Bookstore,
+                    shelf=shelf
+            )
+        else:
+            return render_template('bookstore.html',
+                    sidebar=1,
+                    Bookstore=Bookstore
+            )
     else:
-        return render_template('bookstore.html',
-                sidebar=1,
-                Bookstore=Bookstore
-        )
+        searchText=request.form.get('searchText')
+        Bookstore=Book.query.order_by('BookName').all()
+        searchContents=[]
+        for book in Bookstore:
+            if searchText in book.BookName or searchText in book.Writer:
+                searchContents.append(book)
+        if 'username' in session:
+            userID=User.query.filter(User.UserName==session['username']).first().UserID
+            bookshelf=Bookshelf.query.filter(Bookshelf.UserID==userID)
+            shelf=[]
+            for i in bookshelf:
+                bookname=Book.query.filter(Book.BookID==i.BookID).first().BookName
+                shelf.append(bookname)
+            return render_template('bookstore.html',
+                    sidebar=1,
+                    username=session['username'],
+                    authority=session['authority'],
+                    Bookstore=searchContents,
+                    shelf=shelf
+            )
+        else:
+            return render_template('bookstore.html',
+                    sidebar=1,
+                    Bookstore=searchContents
+            )
 
 @app.route('/signin', methods=['GET','POST'])
 def signin():
@@ -110,6 +144,52 @@ def logout():
         session.pop('username')
         session.pop('authority')
     return redirect('/')
+
+@app.route('/add_book/<bookID>')
+def add_book(bookID):
+    if 'username' in session:
+        userID=User.query.filter(User.UserName==session['username']).first().UserID
+        if Bookshelf.query.count():
+            maxID = Bookshelf.query.order_by(desc('ShelfID')).first().ShelfID
+        else:
+            maxID=0
+        shelf=Bookshelf(ShelfID=maxID+1,
+            UserID=userID,
+            BookID=bookID
+        )
+        db.session.add(shelf)
+        db.session.commit()
+        return redirect('/')
+    else:
+        return render_template('404.html'), 404
+
+@app.route('/myshelf')
+def myshelf():
+    if 'username' in session:
+        userID=User.query.filter(User.UserName==session['username']).first().UserID
+        if userID:
+            shelf=Bookshelf.query.filter(Bookshelf.UserID==userID)
+            books=[]
+            read={}
+            favor={}
+            for i in shelf:
+                books.append(Book.query.filter(Book.BookID==i.BookID).first())
+                read[i.BookID]=i.Read
+                favor[i.BookID]=i.Favor
+            return render_template('shelf.html',
+                    sidebar=2,
+                    username=session['username'],
+                    authority=session['authority'],
+                    Bookshelf=books,
+                    read=read,
+                    favor=favor
+            )
+        else:
+            session.pop('username')
+            session.pop('authority')
+            return render_template('404.html'), 404
+    else:
+        return render_template('404.html'), 404
 
 @app.errorhandler(404)
 def page_not_found(e):
